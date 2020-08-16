c527eba7d96b0959f5cafd3d1e426a4350a3487c
yamazaki0305 <yamazaki0305@hotmail.com>
2020-08-16 17:29:06 +0900

    波のアニメを追加

--- Assets/Scripts/HomeSceneController.cs
+++ Assets/Scripts/HomeSceneController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class HomeSceneController : MonoBehaviour
 {
@@ -20,15 +21,103 @@ public class HomeSceneController : MonoBehaviour
 
 
     Image dogimg;
+    Image bg_img, wave_img;
     float change_time;
+    private int now_hour;
 
     // Start is called before the first frame update
     void Start()
     {
-        dogimg = GameObject.Find("PandaDogImg").GetComponent<Image>();
-        dogimg.sprite = Resources.Load<Sprite>("LessonIMG/" + panda_no[0]);
 
         change_time = 3.5f;
+
+        now_hour = DateTime.Now.Hour;
+        Debug.Log("hour:" + now_hour);
+
+        now_hour = 0;
+
+        if ( 0 <= now_hour && now_hour < 5 )
+        {
+            bg_img = GameObject.Find("Background").GetComponent<Image>();
+            bg_img.sprite = Resources.Load<Sprite>("StageIMG/beach_night");
+
+            wave_img = GameObject.Find("Wave_anime").GetComponent<Image>();
+            wave_img.sprite = Resources.Load<Sprite>("StageIMG/beach_night");
+
+            panda_no[0] = 1;
+            panda_no[1] = 1;
+            panda_no[2] = 1;
+            panda_no[3] = 1;
+            panda_no[4] = 1;
+            panda_no[5] = 1;
+            panda_no[6] = 1;
+            panda_no[7] = 1;
+            panda_text[0] = "aaaaaa";
+            panda_text[1] = "aaaaaa";
+            panda_text[2] = "aaaaaa";
+            panda_text[3] = "aaaaaa";
+            panda_text[4] = "aaaaaa";
+            panda_text[5] = "aaaaaa";
+            panda_text[6] = "aaaaaa";
+            panda_text[7] = "aaaaaa";
+        }
+        else if (5 <= now_hour && now_hour < 7)
+        {
+            bg_img = GameObject.Find("Background").GetComponent<Image>();
+            bg_img.sprite = Resources.Load<Sprite>("StageIMG/beach_sunset");
+
+            wave_img = GameObject.Find("Wave_anime").GetComponent<Image>();
+            wave_img.sprite = Resources.Load<Sprite>("StageIMG/beach_sunset");
+        }
+        else if (7 <= now_hour && now_hour < 16)
+        {
+            bg_img = GameObject.Find("Background").GetComponent<Image>();
+            bg_img.sprite = Resources.Load<Sprite>("StageIMG/beach_m");
+
+            wave_img = GameObject.Find("Wave_anime").GetComponent<Image>();
+            wave_img.sprite = Resources.Load<Sprite>("StageIMG/beach_m");
+        }
+        else if (16 <= now_hour && now_hour < 19)
+        {
+            bg_img = GameObject.Find("Background").GetComponent<Image>();
+            bg_img.sprite = Resources.Load<Sprite>("StageIMG/beach_sunset");
+
+            wave_img = GameObject.Find("Wave_anime").GetComponent<Image>();
+            wave_img.sprite = Resources.Load<Sprite>("StageIMG/beach_sunset");
+
+            panda_no[0] = 1;
+            panda_no[1] = 1;
+            panda_no[2] = 1;
+            panda_no[3] = 1;
+            panda_no[4] = 1;
+            panda_no[5] = 1;
+            panda_no[6] = 1;
+            panda_no[7] = 1;
+            panda_text[0] = "aaaaaa";
+            panda_text[1] = "aaaaaa";
+            panda_text[2] = "aaaaaa";
+            panda_text[3] = "aaaaaa";
+            panda_text[4] = "aaaaaa";
+            panda_text[5] = "aaaaaa";
+            panda_text[6] = "aaaaaa";
+            panda_text[7] = "aaaaaa";
+
+
+
+        }
+        else if (19 <= now_hour && now_hour <= 24)
+        {
+            bg_img = GameObject.Find("Background").GetComponent<Image>();
+            bg_img.sprite = Resources.Load<Sprite>("StageIMG/beach_night");
+
+            wave_img = GameObject.Find("Wave_anime").GetComponent<Image>();
+            wave_img.sprite = Resources.Load<Sprite>("StageIMG/beach_night");
+        }
+
+        dogimg = GameObject.Find("PandaDogImg").GetComponent<Image>();
+        dogimg.sprite = Resources.Load<Sprite>("LessonIMG/" + panda_no[0]);
+        fukidashi_text.text = panda_text[change_no];
+
     }
 
     // Update is called once per frame
